dfc68b2346e8c9978a7d7e5619daeae7dd733e7a
Mahesh Khanvilkar <maheshkhanvilkar@Maheshs-MBP.attlocal.net>
2021-12-30 15:52:48 -0500

    kubernetes pod

--- src/test/java/com/test/api/UserControllerSpec.groovy
+++ src/test/java/com/test/api/UserControllerSpec.groovy
@@ -1,3 +1,35 @@
 package com.test.api
 
+import com.test.service.UserServiceI
+import org.mockito.Mockito
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.MockMvcBuilder
+import org.springframework.test.web.servlet.setup.MockMvcBuilders
+import spock.lang.Specification
+
+
+class UserControllerSpec extends Specification {
+    MockMvc mockMvc
+    UserController userController
+    UserServiceI userServiceI;
+
+    def setup() {
+        userServiceI= Mockito.mock(UserServiceI.class)
+        userController = new UserController(userServiceI)
+        this.mockMvc = MockMvcBuilders.standaloneSetup(userController).build()
+    }
+
+    def whenGetUserAPIIsCalled_returnOKAndUser() {
+        given: "User APIs are available to use"
+        //Mockito.doReturn()
+
+        when: "getUser API is called"
+
+
+        then: "it should return user and status 200"
+
+    }
+
+
+}
 
